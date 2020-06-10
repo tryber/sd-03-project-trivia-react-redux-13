49d1986286b98c9b70b96c54c45c8387e804ec40
Luma Arruda <luma.oarruda@gmail.com>
2020-06-10 14:20:29 -0300

    code climate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Login from "./pages/Login"
-import Game from "./pages/Game"
-import Feedback from "./pages/Feedback"
-import Ranking from "./pages/Ranking"
-import Settings from "./pages/Settings"
+import Login from './pages/Login';
+import Game from './pages/Game';
+import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
+import Settings from './pages/Settings';
 
 function App() {
   return (
