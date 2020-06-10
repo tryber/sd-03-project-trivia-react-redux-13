ecdeb2df759777abcd36376ccdbb448d385ffa2c
Wenderson belko <57719936+wendersonbelko@users.noreply.github.com>
2020-06-10 19:10:49 -0300

    teste

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,35 +3,74 @@ import React from 'react';
 import Logo from '../../trivia.png';
 import './style.css';
 
-function Login() {
-  return (
-    <div className="flexbox">
-      <div className="uk-vertical-align-middle size">
-        <img className="uk-margin-bottom" width="306" height="104" src={Logo} alt="logo" />
-        <form className="uk-panel uk-panel-box uk-form">
-          <div className="uk-form-row">
-            <input
-              className="uk-width-1-1 uk-form-large"
-              type="email"
-              placeholder="E-mail"
-            />
-          </div>
-          <div className="uk-form-row">
-            <input
-              className="uk-width-1-1 uk-form-large"
-              type="text"
-              placeholder="Nome"
-            />
-          </div>
-          <div className="uk-form-row">
-            <button className="uk-button uk-button-secondary uk-width-1-1">
-              entrar
-            </button>
-          </div>
-        </form>
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 0,
+      email: 0,
+    };
+  }
+
+  render() {
+    let name = this.state.name;
+    let email = this.state.email;
+    return (
+      <div className="flexbox">
+        <div className="uk-vertical-align-middle size">
+          <img
+            className="uk-margin-bottom"
+            width="306"
+            height="104"
+            src={Logo}
+            alt="logo"
+          />
+          <form className="uk-panel uk-panel-box uk-form">
+            <div className="uk-form-row">
+              <input
+                onInput={(e) => this.setState({ email: e.target.value })}
+                data-testid="input-gravatar-email"
+                className="uk-width-1-1 uk-form-large"
+                type="email"
+                placeholder="E-mail"
+              />
+            </div>
+            <div className="uk-form-row">
+              <input
+                onInput={(e) => this.setState({ name: e.target.value })}
+                data-testid="input-player-name"
+                className="uk-width-1-1 uk-form-large"
+                type="text"
+                placeholder="Nome"
+              />
+            </div>
+            <div className="uk-form-row">
+              {email < 3 || name < 1 ? (
+                <div className="uk-form-row">
+                  <button
+                    disabled
+                    className="uk-button uk-button-secondary uk-width-1-1"
+                    data-testid="btn-play"
+                  >
+                    entrar
+                  </button>
+                </div>
+              ) : (
+                <div className="uk-form-row">
+                  <button
+                    className="uk-button uk-button-secondary uk-width-1-1"
+                    data-testid="btn-play"
+                  >
+                    entrar
+                  </button>
+                </div>
+              )}
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Login;
