0c1b478351574b8c415c5ab8e26a2efaa2682532
Wenderson belko <57719936+wendersonbelko@users.noreply.github.com>
2020-06-09 22:11:45 -0300

    teste

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -5,13 +5,13 @@ import Test from './imgtest.jpg';
 function NavBar() {
   return (
     <nav>
-      <div className='left'>
+      <div className="left">
         <h3>
-          <img src={Test} className='img' alt='avatar' />
+          <img src={Test} className="img" alt="avatar" />
           Jogador
         </h3>
       </div>
-      <div className='right'>
+      <div className="right">
         <h3>{10 + 10} Pontos</h3>
       </div>
     </nav>
--- src/components/question/index.jsx
+++ src/components/question/index.jsx
@@ -4,10 +4,10 @@ import './style.css';
 function Question() {
   return (
     <div>
-      <div className='uk-child-width-1-3@m uk-grid-small uk-grid-match'>
+      <div className="uk-child-width-1-3@m uk-grid-small uk-grid-match">
         <div>
-          <div className='uk-card uk-card-default uk-card-body border'>
-            <h3 className='uk-card-title'>Titulo</h3>
+          <div className="uk-card uk-card-default uk-card-body border">
+            <h3 className="uk-card-title">Titulo</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
           </div>
         </div>
