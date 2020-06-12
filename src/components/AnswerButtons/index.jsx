14dbd78b908f0d062f77616bf3fd314fa0f3a4d8
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-06-11 21:57:34 -0300

    CC Issues

--- src/components/AnswerButtons/index.jsx
+++ src/components/AnswerButtons/index.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import PropTypes from "prop-types";
-import randomicQuestions from "./randomicQuestions";
-import "./style.css";
-import Button from "./Button";
+import React from 'react';
+import PropTypes from 'prop-types';
+import randomicQuestions from './randomicQuestions';
+import './style.css';
+import Button from './Button';
 
 function AnswersButtons({ data }) {
   if (data !== undefined) {
--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
 import "./style.css";
-import { requestFetch } from "../../action";
 import Navbar from "../../components/Navbar";
 import Question from "../../components/Question";
 import AnswerButtons from "../../components/AnswerButtons";
@@ -16,28 +15,27 @@ class Game extends React.Component {
 
   nextQuestion(limit) {
     this.setState((state) => ({
-      index: state.index + 1 % limit,
-    }))
-  }
-
-  componentDidMount() {
-    this.props.getData();
+      index: state.index + (1 % limit),
+    }));
   }
 
   render() {
     const { data } = this.props;
     const { index } = this.state;
-    if(data !== undefined) return (
+    return (
       <div className="flexbox">
         <Navbar />
-        <Question data={data[index]}/>
+        <Question data={data[index]} />
         <AnswerButtons data={data[index]} />
-        <button type="button" className="button" onClick={() => this.nextQuestion(data.length)}>
+        <button
+          type="button"
+          className="button"
+          onClick={() => this.nextQuestion(data.length)}
+        >
           PRÓXIMA PERGUNTA
         </button>
       </div>
     );
-    return <p>Loading...</p>
   }
 }
 
@@ -45,8 +43,4 @@ const mapStateToProps = (state) => ({
   data: state.request.data,
 });
 
-const mapDispacthToProps = (dispatch) => ({
-  getData: () => dispatch(requestFetch()),
-});
-
-export default connect(mapStateToProps, mapDispacthToProps)(Game);
+export default connect(mapStateToProps)(Game);
