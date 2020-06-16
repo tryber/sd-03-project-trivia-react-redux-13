b0a0ac198e3f9da6e9a87e31a938c387aa7ed9dc
Luma Arruda <luma.oarruda@gmail.com>
2020-06-16 16:26:06 -0300

    mudança no corpo do feedback, requisito de informaçoes de pontuaçao

--- src/pages/Feedback/index.jsx
+++ src/pages/Feedback/index.jsx
@@ -20,8 +20,16 @@ class Feedback extends React.Component {
           <Navbar />
           <h2 data-testid="feedback-text">{answerFeedback}</h2>
           <p> Pontuação:</p>
-          <p>Você fez <span data-testid="feedback-total-score">{score}</span> pontos</p>
-          <p>...e acertou <span data-testid="feedback-total-question">{assertions}</span> pergunta(s)!</p>
+          <p>
+            Você fez 
+            <span data-testid="feedback-total-score">{score}</span>
+            pontos
+          </p>
+          <p>
+            ...e acertou
+            <span data-testid="feedback-total-question">{assertions}</span>
+            pergunta(s)!
+          </p>
           <section>
             <div >
               <GetLinks />
