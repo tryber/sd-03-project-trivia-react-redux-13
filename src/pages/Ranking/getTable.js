bb4911f5d9dc06a93372bcd436fe41cdc413e295
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-06-23 14:04:25 -0300

    CC Issues

--- src/pages/Ranking/getTable.js
+++ src/pages/Ranking/getTable.js
@@ -14,14 +14,21 @@ export const tableBody = (players) => (
   <tbody>
     {players
       .sort((a, b) => b.score - a.score)
-      .map((player, index) => (
-        <tr key={`${player.name}-${index}`}>
-          <th>
-            <img className="ranking-img" src={player.picture} alt={player.name} />
-          </th>
-          <th data-testid={`player-name-${index}`}>{player.name}</th>
-          <th data-testid={`player-score-${index}`}>{player.score}</th>
-        </tr>
-      ))}
+      .map((player, index) => {
+        const key = `${player.name}-${index}`;
+        return (
+          <tr key={key}>
+            <th>
+              <img
+                className="ranking-img"
+                src={player.picture}
+                alt={player.name}
+              />
+            </th>
+            <th data-testid={`player-name-${index}`}>{player.name}</th>
+            <th data-testid={`player-score-${index}`}>{player.score}</th>
+          </tr>
+        );
+      })}
   </tbody>
 );
